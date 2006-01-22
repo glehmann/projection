27a1d0dac5fa9061eb7717ba9525e09e73343a97
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-22 20:18:29 +0100

    add tests to verify that accumulate filter produce the same result
    darcs-hash:20060122191829-2fc9d-626c83399e94de37e53e2fef25a59eb6dcf27238.gz

---
+++ accumulate.cxx
@@ -0,0 +1,36 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCommand.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkAccumulateImageFilter.h"
+
+
+int main(int, char * argv[])
+{
+  const int dim = 3;
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[2] );
+
+  typedef itk::AccumulateImageFilter< IType, IType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->SetAverage( atoi(argv[1]) );
+
+  // there is no progress with this filter !
+  // itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( argv[3] );
+  writer->Update();
+
+  return 0;
+}
+
