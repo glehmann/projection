c48e2df0024a8852ec9dc1440d83dda7ffe581ad
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-22 19:14:57 +0100

    add sum projection and fix tests
    darcs-hash:20060122181457-2fc9d-8d16988b6e0651c342fe4606ce698cb159b8041d.gz

---
+++ itkSumProjectionImageFilter.h
@@ -0,0 +1,75 @@
+#ifndef __itkSumProjectionImageFilter_h
+#define __itkSumProjectionImageFilter_h
+
+#include "itkProjectionImageFilter.h"
+#include "itkNumericTraits.h"
+
+namespace itk {
+/** \class SumProjectionImageFilter
+ * \brief TODO
+ *
+ * \sa ProjectionImageFilter
+ */
+
+
+namespace Function {
+template <class TInputPixel, class TOuputPixel>
+class SumAccumulator
+{
+public:
+  SumAccumulator()
+    {
+    m_Sum = NumericTraits< TInputPixel >::NonpositiveMin();
+    }
+  ~SumAccumulator(){}
+
+  inline TInputPixel operator()( const TInputPixel &input )
+    {
+    m_Sum = m_Sum + input;
+    }
+
+  inline TOuputPixel GetValue()
+    {
+    return m_Sum;
+    }
+
+  TOuputPixel m_Sum;
+};
+} // end namespace Function
+
+
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT SumProjectionImageFilter :
+    public
+    ProjectionImageFilter<TInputImage, TOutputImage,
+      Function::SumAccumulator< typename TInputImage::PixelType, typename TOutputImage::PixelType > >
+{
+public:
+  typedef SumProjectionImageFilter Self;
+  typedef ProjectionImageFilter<TInputImage, TOutputImage, 
+    Function::SumAccumulator< typename TInputImage::PixelType, typename TOutputImage::PixelType > > Superclass;
+
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Runtime information support. */
+  itkTypeMacro(SumProjectionImageFilter, ProjectionImageFilter);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+
+protected:
+  SumProjectionImageFilter() {}
+  virtual ~SumProjectionImageFilter() {}
+
+private:
+  SumProjectionImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+
+
+}; // end SumProjectionImageFilter
+
+} //end namespace itk
+#endif
