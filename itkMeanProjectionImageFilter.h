25a3f7bf497584d52f8aee340fa9eab19c979e9e
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-22 19:32:57 +0100

    add mean projection and tests
    darcs-hash:20060122183257-2fc9d-22f6515b1ac2800c4f5e62ad15481c1ef4d89f11.gz

---
+++ itkMeanProjectionImageFilter.h
@@ -0,0 +1,78 @@
+#ifndef __itkMeanProjectionImageFilter_h
+#define __itkMeanProjectionImageFilter_h
+
+#include "itkProjectionImageFilter.h"
+#include "itkNumericTraits.h"
+
+namespace itk {
+/** \class MeanProjectionImageFilter
+ * \brief TODO
+ *
+ * \sa ProjectionImageFilter
+ */
+
+
+namespace Function {
+template <class TInputPixel, class TOuputPixel>
+class SumAccumulator
+{
+public:
+  SumAccumulator()
+    {
+    m_Sum = NumericTraits< TInputPixel >::Zero;
+    m_Count = 0;
+    }
+  ~SumAccumulator(){}
+
+  inline TInputPixel operator()( const TInputPixel &input )
+    {
+    m_Sum = m_Sum + input;
+    m_Count++;
+    }
+
+  inline TOuputPixel GetValue()
+    {
+    return static_cast<TOuputPixel>( m_Sum / m_Count );
+    }
+
+  typename NumericTraits< TOuputPixel >::AccumulateType m_Sum;
+  unsigned long m_Count;
+};
+} // end namespace Function
+
+
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT MeanProjectionImageFilter :
+    public
+    ProjectionImageFilter<TInputImage, TOutputImage,
+      Function::SumAccumulator< typename TInputImage::PixelType, typename TOutputImage::PixelType > >
+{
+public:
+  typedef MeanProjectionImageFilter Self;
+  typedef ProjectionImageFilter<TInputImage, TOutputImage, 
+    Function::SumAccumulator< typename TInputImage::PixelType, typename TOutputImage::PixelType > > Superclass;
+
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Runtime information support. */
+  itkTypeMacro(MeanProjectionImageFilter, ProjectionImageFilter);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+
+protected:
+  MeanProjectionImageFilter() {}
+  virtual ~MeanProjectionImageFilter() {}
+
+private:
+  MeanProjectionImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+
+
+}; // end MeanProjectionImageFilter
+
+} //end namespace itk
+#endif
