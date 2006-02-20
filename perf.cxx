89c324b1101ccf6c91c3b89e75968a945641cf54
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-02-21 00:23:00 +0100

    only measure perf for max
    darcs-hash:20060220232300-2fc9d-b911471b05e5751a99570ecd66ab86f1901d5b68.gz

--- perf.cxx
+++ perf.cxx
@@ -31,132 +31,45 @@ int main(int, char * argv[])
   MaxType::Pointer max = MaxType::New();
   max->SetInput( reader->GetOutput() );
   
-  typedef itk::MinimumProjectionImageFilter< IType, LIType > MinType;
-  MinType::Pointer min = MinType::New();
-  min->SetInput( reader->GetOutput() );
-  
-  typedef itk::SumProjectionImageFilter< IType, LIType > SumType;
-  SumType::Pointer sum = SumType::New();
-  sum->SetInput( reader->GetOutput() );
-  
-  typedef itk::MeanProjectionImageFilter< IType, LIType > MeanType;
-  MeanType::Pointer mean = MeanType::New();
-  mean->SetInput( reader->GetOutput() );
-  
-  typedef itk::MedianProjectionImageFilter< IType, LIType > MedianType;
-  MedianType::Pointer median = MedianType::New();
-  median->SetInput( reader->GetOutput() );
-  
-  typedef itk::SigmaProjectionImageFilter< IType, LIType > SigmaType;
-  SigmaType::Pointer sigma = SigmaType::New();
-  sigma->SetInput( reader->GetOutput() );
-  
-  typedef itk::BinaryProjectionImageFilter< IType, LIType > BinaryType;
-  BinaryType::Pointer binary = BinaryType::New();
-  binary->SetInput( reader->GetOutput() );
-  
   reader->Update();
   
   
   std::cout << "#th" << "\t" 
             << "max" << "\t" 
-            << "min" << "\t" 
-            << "sum" << "\t" 
-            << "mean" << "\t" 
-            << "median" << "\t" 
-            << "sigma" << "\t" 
-            << "binary" << "\t" 
             << std::endl;
 
-    for( int th=1; th<=10; th++ )
+  const int rep1 = 50;
+  const int rep2 = 100;
+  const int maxNbOfThreads = 10;
+
+  std::vector< itk::TimeProbe > timeProbes;
+  timeProbes.resize( maxNbOfThreads );
+
+  for( int r1=0; r1<=rep1; r1++ )
+    {
+
+    for( int th=0; th<maxNbOfThreads; th++ )
       {
-      itk::TimeProbe maxtime;
-      itk::TimeProbe mintime;
-      itk::TimeProbe sumtime;
-      itk::TimeProbe meantime;
-      itk::TimeProbe sigmatime;
-      itk::TimeProbe mediantime;
-      itk::TimeProbe binarytime;
+      max->SetNumberOfThreads( th+1 );
 
-      max->SetNumberOfThreads( th );
-      min->SetNumberOfThreads( th );
-      sum->SetNumberOfThreads( th );
-      mean->SetNumberOfThreads( th );
-      median->SetNumberOfThreads( th );
-      sigma->SetNumberOfThreads( th );
-      binary->SetNumberOfThreads( th );
-
-      for( int i=0; i<10; i++ )
+      timeProbes[th].Start();
+      for( int r2=0; r2<rep2; r2++ )
         {
-        maxtime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          max->Update();
-          max->Modified();
-          }
-        maxtime.Stop();
-  
-        mintime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          min->Update();
-          min->Modified();
-          }
-        mintime.Stop();
-  
-        sumtime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          sum->Update();
-          sum->Modified();
-          }
-        sumtime.Stop();
-  
-        meantime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          mean->Update();
-          mean->Modified();
-          }
-        meantime.Stop();
-  
-        mediantime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          median->Update();
-          median->Modified();
-          }
-        mediantime.Stop();
-  
-        sigmatime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          sigma->Update();
-          sigma->Modified();
-          }
-        sigmatime.Stop();
-  
-        binarytime.Start();
-        for( int i=0; i<10; i++ )
-          {
-          binary->Update();
-          binary->Modified();
-          }
-        binarytime.Stop();
-  
+        max->Update();
+        max->Modified();
         }
-        
-      std::cout << std::setprecision(3)
-                << th << "\t" 
-                << maxtime.GetMeanTime() / 10 << "\t" 
-                << mintime.GetMeanTime() / 10 << "\t" 
-                << sumtime.GetMeanTime() / 10 << "\t" 
-                << meantime.GetMeanTime() / 10 << "\t" 
-                << mediantime.GetMeanTime() / 10 << "\t" 
-                << sigmatime.GetMeanTime() / 10 << "\t" 
-                << binarytime.GetMeanTime() / 10 << "\t" 
-                <<std::endl;
+      timeProbes[th].Stop();
+
       }
+    }
+
+  for( int th=0; th<maxNbOfThreads; th++ )
+    {
+    std::cout << std::setprecision(3)
+              << th+1 << "\t" 
+              << timeProbes[th].GetMeanTime() / rep2 << "\t" 
+              <<std::endl;
+    }
   
   return 0;
 }
